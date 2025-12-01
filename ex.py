2f3d847715b830e82078259d2e17d78ff3ce1811
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2025-12-01 06:57:57 +0000

    Exercício 2: Classificador de idade

--- ex.py
+++ ex.py
@@ -4,3 +4,14 @@ if n % 2 == 0:
     print(f"{n} é par")
 else:
     print(f"{n} é ímpar")
+
+    idade = int(input("Digite a idade: "))
+
+if idade < 12:
+    print("criança")
+elif idade <= 17:
+    print("adolescente")
+elif idade <= 59:
+    print("adulto")
+else:
+    print("idoso")
